9dba1806e4403a7dc04a1921d073f35989e9dc3d
AMOREM <amorem@amorem.com>
2019-08-29 22:49:51 +0200

    Location bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<String> _location = ['Japan', 'Moscow', 'London', 'Paris'];
+  int _activeLocation = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,45 +18,92 @@ class _HomePageState extends State<HomePage> {
         bottom: true,
         minimum: EdgeInsets.fromLTRB(MediaQuery.of(context).size.width * 0.05,
             0, MediaQuery.of(context).size.width * 0.05, 0),
-        child: _mainColumn(),
+        child: _mainColumn(context),
       ),
     );
   }
-}
 
-Widget _mainColumn() {
-  return Column(
-    mainAxisAlignment: MainAxisAlignment.start,
-    mainAxisSize: MainAxisSize.max,
-    crossAxisAlignment: CrossAxisAlignment.center,
-    children: <Widget>[
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Icon(
-            Icons.menu,
-            color: Colors.black87,
-            size: 35.0,
-          ),
-          Container(
-            height: 39,
-            width: 144,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.fill,
-                image: AssetImage('assets/images/logo_discover.png'),
+  Widget _mainColumn(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Icon(
+              Icons.menu,
+              color: Colors.black87,
+              size: 35.0,
+            ),
+            Container(
+              height: 39,
+              width: 144,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage('assets/images/logo_discover.png'),
+                ),
               ),
             ),
-          ),
-          Icon(
-            Icons.search,
-            color: Colors.black87,
-            size: 35.0,
-          ),
-        ],
-      )
-    ],
-  );
+            Icon(
+              Icons.search,
+              color: Colors.black87,
+              size: 35.0,
+            ),
+          ],
+        ),
+        Padding(
+          padding:
+              EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.03),
+          child: _locationBar(context),
+        ),
+      ],
+    );
+  }
+
+  Widget _locationBar(BuildContext _context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.065,
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(69, 69, 69, 1),
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: _location.map(
+          (f) {
+            bool _isActive = _location[_activeLocation] == f ? true : false;
+            return Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text(
+                  f,
+                  style: TextStyle(
+                      fontSize: 15,
+                      color: _isActive ? Colors.white : Colors.white54,
+                      fontFamily: 'Montserrat'),
+                ),
+                _isActive
+                    ? Container(
+                        height: 5,
+                        width: 30,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.redAccent,
+                        ),
+                      )
+                    : Container(),
+              ],
+            );
+          },
+        ).toList(),
+      ),
+    );
+  }
 }
